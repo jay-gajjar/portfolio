62d042ec5cae89ea50b46956a561e817ca43c9ea
Jay Gajjar <gajjarjay1711@gmail.com>
2026-03-07 14:05:12 +0530

    added analytics

--- src/app/components/info/info.ts
+++ src/app/components/info/info.ts
@@ -2,10 +2,21 @@ import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 
+declare let gtag: any;
+
 @Component({
   selector: 'app-info',
   imports: [MatButtonModule, NgOptimizedImage],
   templateUrl: './info.html',
   styleUrl: './info.scss',
 })
-export class Info {}
+export class Info {
+
+  downloadResume() {
+    console.log('hii');
+    gtag('event', 'resume_download', {
+      event_category: 'engagement',
+      event_label: 'resume pdf'
+    });
+  }
+}
